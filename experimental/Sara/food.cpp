7d652c8b0a619615b5c5702aa5bbe12d7ba761f1
cloudwaysX <yifang@usc.edu>
2015-09-26 17:43:28 -0700

    food

---
+++ experimental/Sara/food.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : USC.cpp
+// Author      : Sara
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Yifang Chen" << endl; // prints !!!Hello World!!!
+	cout << "Let's vote for food"<<endl;
+	return 0;
+}
