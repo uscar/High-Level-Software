f975c653a6237bd1f45f7e98886a64413b5543f8
Nolan Miller <namiller@usc.edu>
2015-11-30 18:03:14 -0800

    added collision time handling

--- experimental/namiller/collision_detection.cpp
+++ experimental/namiller/collision_detection.cpp
@@ -22,31 +22,46 @@ state state_at_time(roomba r, double t) {
 }
 
 // returns time of collision or -1 if no such time
-bool does_collide(roomba r1, roomba r2, double epoch, double res = .01) {
+bool does_collide(roomba r1, roomba r2, double epoch,double t0 = 0, double res = .01) {
     double dt = epoch/2.0;
-    double t = 0;
+    double t = t0;
     state s1, s2;
-    while (dt > res && t < 0;) {
+    while (dt > res && t < t0;) {
         s1 = state_at_time(r1, t);
         s2 = state_at_time(r2, t);
         int fact = 1;
         if ((s1.pos - s2.pos).dot(s1.vel - s2.vel) > 0) //make sure this is really gt not lt
             fact *= -1;
-        t += dt;
+        t += dt*fact;
         dt /= 2;
     }
-    if (t < 0 || t >= epoch - res)
-        return false;
-    return s1.pos.dist(s2.pos) < RADIUS;
+    
+    if (t < t0 || t >= epoch - res || s1.pos.dist(s2.pos) > RADIUS)
+        return -1;
+  
+    dt = epoch/2.0; 
+    while (dt > res) {
+        s1 = state_at_time(r1, t);
+        s2 = state_at_time(r2, t);
+        int fact = 1;
+        if (s1.pos.dist(s2.pos) < RADIUS)
+            fact *= -1;
+        t += dt*fact;
+        dt /= 2;
+    }
+    return t;
 }
 
-// returns the index of the first roomba it collides with or -1 if none.
-int does_collide(roomba r, vector<roomba> others, double epoch) {
+// returns the first collision or -1 if no collisions
+double does_collide(roomba r, vector<roomba> others, double epoch, double t0 = 0, double res = .01) {
+    const double m = 100000;
+    double collision = m;
+    double tmp;
     for (int i = 0; i < others.size(); i++) {
-        if (does_collide(others[i], r, epoch))
-            return i;
+        if ((tmp = does_collide(others[i], r, epoch, t0, res)) != -1)
+            collision = collision<tmp?collision:tmp;
     }
-    return -1;
+    return collision == m ? -1 : collision;
 }
 
 
